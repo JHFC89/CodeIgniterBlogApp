8ac8e79f65952ffad14c8a2ccc4d21d6327764d5
João Henrique <faber.calarga@gmail.com>
2019-03-18 15:03:40 -0300

    Part 2 added post creation option added post delete option added post edit opition added ckeditor added create post option to navbar

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -5,6 +5,7 @@
     <link rel="stylesheet" type="text/css" href="https://bootswatch.com/4/sketchy/bootstrap.min.css">
 </head>
     <link rel="stylesheet" href="<?php echo base_url(); ?>/assets/css/style.css">
+    <script src="https://cdn.ckeditor.com/ckeditor5/12.0.0/classic/ckeditor.js"></script>
 
 <!-- Navbar -->
 <body>
@@ -13,13 +14,14 @@
             <div class="navbar-header">
                 <a href="<?php echo base_url(); ?>" class="navbar-brand">ciBlog</a>
             </div>
-            <div class="container" id="navbar">
-                <ul class="navbar-nav ml-auto inline">
+                <ul class="navbar-nav mr-auto">
                     <li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>">Home</a></li>
                     <li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>about">About</a></li>
                     <li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>posts">Blog</a></li>
                 </ul>
-            </div>
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a class="nav-link" href="<?php echo base_url(); ?>posts/create">Create Post</a></li>
+                </ul>
         </div>
     </nav>
 
