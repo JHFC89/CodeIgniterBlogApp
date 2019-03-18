488bb3e5b33a4e3f5b4e5a19e18d4399634bd91d
João Henrique <faber.calarga@gmail.com>
2019-03-18 19:58:51 -0300

    Little change Changed the title`s colors of the posts

--- application/views/posts/index.php
+++ application/views/posts/index.php
@@ -1,6 +1,6 @@
 <h2><?= $title ?></h2>
 <?php foreach($posts as $post): ?>
-    <h3><?php echo $post['title']; ?></h3>
+    <h3 class="text-info"><?php echo $post['title']; ?></h3>
     <small class="post-date">Posted on: <?php echo $post['created_at']; ?></small>
     <p><?php echo $post['body']; ?></p>
     <p><a class="btn btn-dark"href="<?php echo site_url('/posts/'.$post['slug'])?>">Read More</a></p>
